8706f886f59090f5f9c1fbf67ac46d423fa01319
aleksandr12smirnov <alexandershni@gmail.com>
2019-05-05 13:29:42 -0700

    back-end

--- smartsheets_backend/how_found_pyx/urls.py
+++ smartsheets_backend/how_found_pyx/urls.py
@@ -3,10 +3,6 @@ from rest_framework import routers
 from .views import HowFoundPyxList, HowFoundPyxDetailView
 
 urlpatterns = [
-<<<<<<< HEAD
-    
-=======
     url(r'^how_found_pyx/(?P<pk>\d+)/', HowFoundPyxDetailView.as_view()),
     url(r'^how_found_pyx/', HowFoundPyxList.as_view()),
->>>>>>> ee54b5d90031b00806fb29c2095736def343333d
 ]
